4125d165cbc9cb5fd01f4af1853646b8c8e30cf5
Štěpán <tpn@DESKTOP-5KPURFS>
2026-05-17 18:25:14 +0200

    Add court create and delete endpoints

--- src/main/java/cz/school/tenniscourtreservation/controller/CourtController.java
+++ src/main/java/cz/school/tenniscourtreservation/controller/CourtController.java
@@ -2,9 +2,9 @@ package cz.school.tenniscourtreservation.controller;
 
 import cz.school.tenniscourtreservation.model.Court;
 import cz.school.tenniscourtreservation.service.CourtService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,5 +21,17 @@ public class CourtController {
     @GetMapping
     public List<Court> getAllCourts() {
         return courtService.getAllCourts();
+    }
+
+    @PostMapping
+    public ResponseEntity<Court> createCourt(@RequestBody Court court) {
+        Court createdCourt = courtService.createCourt(court);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdCourt);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteCourt(@PathVariable Long id) {
+        courtService.deleteCourt(id);
+        return ResponseEntity.noContent().build();
     }
 }
